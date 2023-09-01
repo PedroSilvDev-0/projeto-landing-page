123adfe71c52e25d074e25eb7b8438db75b6bc91
PedroSilvDev-0 <silvsrhyan@gmail.com>
2023-08-31 23:03:08 -0300

    Versao Inicial

---
+++ index.js
@@ -0,0 +1,19 @@
+var setaDireita = window.document.getElementById("seta-direita")
+var Leonardo = window.document.getElementById("leonardo")
+var Samantha = window.document.getElementById("samantha")
+var Bruna = window.document.getElementById("bruna")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+
+function RolarParaDireita() {
+    Leonardo.style = "display:none"
+    Bruna.style = "display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex; margin-top:55px"
+}
+
+function RolarParaEsquerda() {
+    Bruna.style = "display:none"
+    Leonardo.style = "display:flex"
+    setaEsquerda.style = "display:none"
+    setaDireita.style = "display:flex; margin-top:55px"
+}
